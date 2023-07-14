97ead1d909c90d51a3ee4b4efe1ef33c0119076d
FirefliesDancing <139577055+FirefliesDancing@users.noreply.github.com>
2023-07-14 15:13:10 -0700

    updated survey.py and also created a submit page

--- survey.py
+++ survey.py
@@ -19,11 +19,11 @@ def submit():
     email = request.form["email"]
     uniform = request.form["uniform"]
     patches = request.form["patches"]
-    class_age = request.form["class_age"]
-    attendance = request.form["attendance"]
+
+    return render_template("submitted_index.html")
 
 
 # This is a thing for python that makes it so that if someone calls on this file as a library
 # then they can get the functions without actually running the program (if you have questions text me)
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=80)
